9220fe4d50789d957aa1fdea29de5cad3f60ed91
DESKTOP-BF8P05H\alfam <alfamod47@yahoo.co.jp>
2022-07-21 16:54:18 +0900

    test2

--- Assets/Script/GitHubTest.cs
+++ Assets/Script/GitHubTest.cs
@@ -7,16 +7,14 @@ public class GitHubTest : MonoBehaviour
 
     string s_GitHub = "GitHubTest";
 
-    // Start is called before the first frame update
     void Start()
     {
         Debug.Log(s_GitHub);
-        Debug.Log("s_GitHubTest2");
+        Debug.Log("GitHubTest2");
         Debug.Log("SSHを設定していないのに何故か上手くいく");
         Debug.Log("GitHubTest_2022_07_21");
     }
 
-    // Update is called once per frame
     void Update()
     {
 
