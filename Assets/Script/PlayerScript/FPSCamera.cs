d9931b2fde942446959b1ebf5c3600b6bf33300e
DESKTOP-BF8P05H\alfam <alfamod47@yahoo.co.jp>
2023-11-22 00:23:56 +0900

    コメントの追加

--- Assets/Script/PlayerScript/FPSCamera.cs
+++ Assets/Script/PlayerScript/FPSCamera.cs
@@ -21,12 +21,13 @@ public class FPSCamera : MonoBehaviour
     {
         float x_Rotation = Input.GetAxis("Mouse X");
         float y_Rotation = Input.GetAxis("Mouse Y");
+        //マウスXの入力量 × カメラのスピード × 時間 = の値をX回転の量にする
         playerTransform.transform.Rotate(0, x_Rotation * cameraSpeedX * Time.deltaTime, 0);
 
         cameraAngles = cameraTransform.transform.localEulerAngles.x;
         if (324 < cameraAngles && cameraAngles < 360 || -10 < cameraAngles && cameraAngles < 79)//ここの各左の数字を変えればカメラの上下の止まる限界値が変わる
         {
-
+            //マウスYの入力量 × カメラのスピード × 時間 = の値をY回転の量にする
             cameraTransform.transform.Rotate(-y_Rotation * cameraSpeedY * Time.deltaTime, 0, 0);
 
         }
@@ -36,7 +37,7 @@ public class FPSCamera : MonoBehaviour
             {
                 if (Input.GetAxis("Mouse Y") < 0)
                 {
-
+                    //マウスYの入力量 × カメラのスピード × 時間 = の値をY回転の量にする
                     cameraTransform.transform.Rotate(-y_Rotation * cameraSpeedY * Time.deltaTime, 0, 0);
 
                 }
@@ -45,7 +46,7 @@ public class FPSCamera : MonoBehaviour
             {
                 if (0 < Input.GetAxis("Mouse Y"))
                 {
-
+                    //マウスYの入力量 × カメラのスピード × 時間 = の値をY回転の量にする
                     cameraTransform.transform.Rotate(-y_Rotation * cameraSpeedY * Time.deltaTime, 0, 0);
 
                 }
