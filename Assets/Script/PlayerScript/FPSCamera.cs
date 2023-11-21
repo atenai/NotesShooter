e08f5697dbf5cd6911fdd48598c12c9ae87eba06
DESKTOP-BF8P05H\alfam <alfamod47@yahoo.co.jp>
2023-11-21 23:51:33 +0900

    変数名を修正

--- Assets/Script/PlayerScript/FPSCamera.cs
+++ Assets/Script/PlayerScript/FPSCamera.cs
@@ -2,38 +2,36 @@
 
 public class FPSCamera : MonoBehaviour
 {
-    //横回転(Y)はプレイヤーキャラクターの座標位置を使っている(Player)
     //縦回転(X)はカメラの座標位置を使っている(FPSCamera)
+    //横回転(Y)はプレイヤーキャラクターの座標位置を使っている(Player)
 
-    public GameObject Camera;
-    private Transform PlayerTransform;
-    private Transform CameraTransform;
+
+    [SerializeField] public float cameraSpeedX = 100;
+    [SerializeField] public float cameraSpeedY = 100;
+
+    private GameObject cameraGameObject;
+    private Transform playerTransform;
+    private Transform cameraTransform;
     private float cameraAngles;
-
-    private void Awake()
-    {
-        //マウスカーソルを消す
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
 
     void Start()
     {
-        PlayerTransform = transform.parent;
-        CameraTransform = GetComponent<Transform>();
+        cameraGameObject = this.gameObject;
+        playerTransform = transform.parent;
+        cameraTransform = this.GetComponent<Transform>();
     }
 
     void Update()
     {
-        float X_Rotation = Input.GetAxis("Mouse X");
-        float Y_Rotation = Input.GetAxis("Mouse Y");
-        PlayerTransform.transform.Rotate(0, X_Rotation, 0);
+        float x_Rotation = Input.GetAxis("Mouse X");
+        float y_Rotation = Input.GetAxis("Mouse Y");
+        playerTransform.transform.Rotate(0, x_Rotation * cameraSpeedX * Time.deltaTime, 0);
 
-        cameraAngles = Camera.transform.localEulerAngles.x;
+        cameraAngles = cameraGameObject.transform.localEulerAngles.x;
         if (324 < cameraAngles && cameraAngles < 360 || -10 < cameraAngles && cameraAngles < 79)//ここの各左の数字を変えればカメラの上下の止まる限界値が変わる
         {
 
-            CameraTransform.transform.Rotate(-Y_Rotation, 0, 0);
+            cameraTransform.transform.Rotate(-y_Rotation * cameraSpeedY * Time.deltaTime, 0, 0);
 
         }
         else
@@ -43,7 +41,7 @@ public class FPSCamera : MonoBehaviour
                 if (Input.GetAxis("Mouse Y") < 0)
                 {
 
-                    CameraTransform.transform.Rotate(-Y_Rotation, 0, 0);
+                    cameraTransform.transform.Rotate(-y_Rotation * cameraSpeedY * Time.deltaTime, 0, 0);
 
                 }
             }
@@ -52,7 +50,7 @@ public class FPSCamera : MonoBehaviour
                 if (0 < Input.GetAxis("Mouse Y"))
                 {
 
-                    CameraTransform.transform.Rotate(-Y_Rotation, 0, 0);
+                    cameraTransform.transform.Rotate(-y_Rotation * cameraSpeedY * Time.deltaTime, 0, 0);
 
                 }
 
