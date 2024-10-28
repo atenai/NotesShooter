edc19b672bb3a8ccf7ab8746c9d84314b7335f0a
atenai <alfamod47@yahoo.co.jp>
2024-10-29 07:32:13 +0900

    カメラを関数化し、修正

--- Assets/NotesShooter/Script/PlayerScript/FPSCamera.cs
+++ Assets/NotesShooter/Script/PlayerScript/FPSCamera.cs
@@ -2,9 +2,9 @@
 
 public class FPSCamera : MonoBehaviour
 {
-    // 最小値は50、最大値は150としてスライダーを表示
+    //最小値は50、最大値は150としてスライダーを表示
     [Range(50, 150)] public float cameraSpeedX = 100;
-    // 最小値は50、最大値は150としてスライダーを表示
+    //最小値は50、最大値は150としてスライダーを表示
     [Range(50, 150)] public float cameraSpeedY = 100;
 
     [Tooltip("横回転(Y)はプレイヤーキャラクターの座標位置を使っている(Player_RotY)")]
@@ -22,13 +22,24 @@ public class FPSCamera : MonoBehaviour
     {
         float x_Rotation = Input.GetAxis("Mouse X");
         float y_Rotation = Input.GetAxis("Mouse Y");
+        MouseCamera(new Vector2(x_Rotation, y_Rotation));
+    }
+
+    void MouseCamera(Vector2 angles)
+    {
+        float x_Rotation = angles.x;
+        float y_Rotation = angles.y;
+
         //マウスXの入力量 × カメラのスピード × 時間 = の値をX回転の量にする
         playerTransform.transform.Rotate(0, x_Rotation * cameraSpeedX * Time.deltaTime, 0);
 
         float cameraAngles = cameraTransform.transform.localEulerAngles.x;
-        const float lookingDownLimit = 79.0f;
-        const float lookingUpLimit = 360.0f;
-        if (324 < cameraAngles && cameraAngles < lookingUpLimit || -10 < cameraAngles && cameraAngles < lookingDownLimit)//ここの各左の数字を変えればカメラの上下の止まる限界値が変わる
+        const float lookingUpLimit = 360.0f;//変えてはいけない数値
+        float lookingUp = 324.0f;//減らしていくほど上を見れる範囲が広がる
+        const float lookingDownLimit = -10.0f;//変えてはいけない数値
+        float lookingDown = 79.0f;//増やしていくほど下を見れる範囲が広がる
+
+        if (lookingUp < cameraAngles && cameraAngles < lookingUpLimit || lookingDownLimit < cameraAngles && cameraAngles < lookingDown)//ここの数値を変えればカメラの上下の止まる限界値が変わる
         {
             //マウスYの入力量 × カメラのスピード × 時間 = の値をY回転の量にする
             cameraTransform.transform.Rotate(-y_Rotation * cameraSpeedY * Time.deltaTime, 0, 0);
@@ -53,6 +64,5 @@ public class FPSCamera : MonoBehaviour
 
             }
         }
-
     }
 }
