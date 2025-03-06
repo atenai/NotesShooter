5601be6a08b66bbc0e102bb3e5bea3d315f1b900
atenai <alfamod47@yahoo.co.jp>
2025-03-06 21:56:14 +0900

    スクロールするまで次の処理は待機するように作成

--- Assets/NotesShooter/Script/StageSelect/ScrollRectExtension.cs
+++ Assets/NotesShooter/Script/StageSelect/ScrollRectExtension.cs
@@ -1,5 +1,6 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 /// <summary>
@@ -7,68 +8,71 @@ using UnityEngine.UI;
 /// </summary>
 public static class ScrollRectExtension
 {
-    /// <summary>
-    /// ScrollRectの上端にGameObjectをあわせる
-    /// </summary>
-    /// <param name="scrollRect"></param>
-    /// <param name="go"></param>
-    public static float ScrollToBeBottom(this ScrollRect scrollRect, GameObject go, float speed)
-    {
-        const float align = 0.0f;
-        return ScrollToCore(scrollRect, go, align, speed);
-    }
+	/// <summary>
+	/// ScrollRectの上端にGameObjectをあわせる
+	/// </summary>
+	/// <param name="scrollRect"></param>
+	/// <param name="go"></param>
+	public static float ScrollToBeBottom(this ScrollRect scrollRect, GameObject go, float speed, UnityAction onComplete = null)
+	{
+		const float align = 0.0f;
+		return ScrollToCore(scrollRect, go, align, speed, onComplete);
+	}
 
-    /// <summary>
-    /// ScrollRectの縦中央にGameObjectをあわせる
-    /// </summary>
-    /// <param name="scrollRect"></param>
-    /// <param name="go"></param>
-    public static float ScrollToCentering(this ScrollRect scrollRect, GameObject go, float speed)
-    {
-        const float align = 0.5f;
-        return ScrollToCore(scrollRect, go, align, speed);
-    }
+	/// <summary>
+	/// ScrollRectの縦中央にGameObjectをあわせる
+	/// </summary>
+	/// <param name="scrollRect"></param>
+	/// <param name="go"></param>
+	public static float ScrollToCentering(this ScrollRect scrollRect, GameObject go, float speed, UnityAction onComplete = null)
+	{
+		const float align = 0.5f;
+		return ScrollToCore(scrollRect, go, align, speed, onComplete);
+	}
 
-    /// <summary>
-    /// ScrollRectの下端にGameObjectをあわせる
-    /// </summary>
-    /// <param name="scrollRect"></param>
-    /// <param name="go"></param>
-    public static float ScrollToBeTop(this ScrollRect scrollRect, GameObject go, float speed)
-    {
-        const float align = 1.0f;
-        return ScrollToCore(scrollRect, go, align, speed);
-    }
+	/// <summary>
+	/// ScrollRectの下端にGameObjectをあわせる
+	/// </summary>
+	/// <param name="scrollRect"></param>
+	/// <param name="go"></param>
+	public static float ScrollToBeTop(this ScrollRect scrollRect, GameObject go, float speed, UnityAction onComplete = null)
+	{
+		const float align = 1.0f;
+		return ScrollToCore(scrollRect, go, align, speed, onComplete);
+	}
 
-    /// <summary>
-    /// ScrollRectのスクロール位置をGameObjectにあわせる
-    /// </summary>
-    /// <param name="scrollRect"></param>
-    /// <param name="go"></param>
-    /// <param name="align">0:下、0.5:中央、1:上</param>
-    /// <returns></returns>
-    static private float ScrollToCore(ScrollRect scrollRect, GameObject go, float align, float speed)
-    {
-        var targetRect = go.transform.GetComponent<RectTransform>();
-        var contentHeight = scrollRect.content.rect.height;
-        var viewportHeight = scrollRect.viewport.rect.height;
-        // スクロール不要
-        if (contentHeight < viewportHeight) return 0f;
+	/// <summary>
+	/// ScrollRectのスクロール位置をGameObjectにあわせる
+	/// </summary>
+	/// <param name="scrollRect"></param>
+	/// <param name="go"></param>
+	/// <param name="align">0:下、0.5:中央、1:上</param>
+	/// <returns></returns>
+	static private float ScrollToCore(ScrollRect scrollRect, GameObject go, float align, float speed, UnityAction onComplete = null)
+	{
+		var targetRect = go.transform.GetComponent<RectTransform>();
+		var contentHeight = scrollRect.content.rect.height;
+		var viewportHeight = scrollRect.viewport.rect.height;
+		// スクロール不要
+		if (contentHeight < viewportHeight) return 0f;
 
-        // ローカル座標が contentHeight の上辺を0として負の値で格納されてる
-        // これは現在のレイアウト特有なのかもしれないので、要確認
-        var targetPos = contentHeight + GetPosY(targetRect) + targetRect.rect.height * align;
-        var gap = viewportHeight * align; // 上端〜下端あわせのための調整量
-        var normalizedPos = (targetPos - gap) / (contentHeight - viewportHeight);
+		// ローカル座標が contentHeight の上辺を0として負の値で格納されてる
+		// これは現在のレイアウト特有なのかもしれないので、要確認
+		var targetPos = contentHeight + GetPosY(targetRect) + targetRect.rect.height * align;
+		var gap = viewportHeight * align; // 上端〜下端あわせのための調整量
+		var normalizedPos = (targetPos - gap) / (contentHeight - viewportHeight);
 
-        normalizedPos = Mathf.Clamp01(normalizedPos);
-        //scrollRect.verticalNormalizedPosition = normalizedPos;
-        DOTween.To(() => scrollRect.verticalNormalizedPosition, (x) => scrollRect.verticalNormalizedPosition = x, normalizedPos, speed).SetEase(Ease.Linear);
-        return normalizedPos;
-    }
+		normalizedPos = Mathf.Clamp01(normalizedPos);
+		//scrollRect.verticalNormalizedPosition = normalizedPos;
+		DOTween.To(() => scrollRect.verticalNormalizedPosition, (x) => scrollRect.verticalNormalizedPosition = x, normalizedPos, speed).SetEase(Ease.Linear).OnComplete(() =>
+		{
+			onComplete?.Invoke();
+		});
+		return normalizedPos;
+	}
 
-    static private float GetPosY(RectTransform transform)
-    {
-        return transform.localPosition.y + transform.rect.y; //pivotによるズレをrect.yで補正
-    }
+	static private float GetPosY(RectTransform transform)
+	{
+		return transform.localPosition.y + transform.rect.y; //pivotによるズレをrect.yで補正
+	}
 }
--- Assets/NotesShooter/Script/StageSelect/StageSelectManager.cs
+++ Assets/NotesShooter/Script/StageSelect/StageSelectManager.cs
@@ -6,87 +6,105 @@ using UnityEngine.UI;
 
 public class StageSelectManager : MonoBehaviour
 {
-    [SerializeField] private ScrollRect scrollRect;
-    [SerializeField] private GameObject content;
-    [Tooltip("ステージボタンのプレハブ")]
-    [SerializeField] private GameObject stageSelectButtonPrefab;
-    [SerializeField] private GameObject bonusStageSelectButtonPrefab;
-    int totalStageCount = 4;
-    List<StageSelectButton> stageSelectButtons = new List<StageSelectButton>();
-    BonusStageSelectButton bonusStageSelectButton;
+	[SerializeField] private ScrollRect scrollRect;
+	[SerializeField] private GameObject content;
+	[Tooltip("ステージボタンのプレハブ")]
+	[SerializeField] private GameObject stageSelectButtonPrefab;
+	[SerializeField] private GameObject bonusStageSelectButtonPrefab;
+	int totalStageCount = 4;
+	List<StageSelectButton> stageSelectButtons = new List<StageSelectButton>();
+	BonusStageSelectButton bonusStageSelectButton;
 
-    public static int playCount = 1;
+	public static int playCount = 1;
 
-    void Start()
-    {
-        CreateStageButtons();
+	void Start()
+	{
+		CreateStageButtons();
 
-        //以下演出
-        scrollRect.verticalNormalizedPosition = 0 / totalStageCount;
+		//以下演出
+		scrollRect.verticalNormalizedPosition = 0 / totalStageCount;
 
-        StartCoroutine(Direction());
-    }
+		StartCoroutine(Direction());
+	}
 
-    void CreateStageButtons()
-    {
-        for (int i = 1; i <= totalStageCount - 1; i++)
-        {
-            // プレハブからボタンをInstantiateしてContentの子オブジェクトに配置
-            GameObject stageSelectButtonGameObject = Instantiate(stageSelectButtonPrefab, new Vector3(0, 0, 0), Quaternion.identity, content.transform);
-            StageSelectButton stageSelectButton = stageSelectButtonGameObject.GetComponent<StageSelectButton>();
-            stageSelectButton.Initialize(i, totalStageCount, playCount);
-            stageSelectButtons.Add(stageSelectButton);
-        }
+	/// <summary>
+	/// ステージボタン生成
+	/// </summary>
+	void CreateStageButtons()
+	{
+		for (int i = 1; i <= totalStageCount - 1; i++)
+		{
+			// プレハブからボタンをInstantiateしてContentの子オブジェクトに配置
+			GameObject stageSelectButtonGameObject = Instantiate(stageSelectButtonPrefab, new Vector3(0, 0, 0), Quaternion.identity, content.transform);
+			StageSelectButton stageSelectButton = stageSelectButtonGameObject.GetComponent<StageSelectButton>();
+			stageSelectButton.Initialize(i, totalStageCount, playCount);
+			stageSelectButtons.Add(stageSelectButton);
+		}
 
-        GameObject bonusStageSelectButtonGameObject = Instantiate(bonusStageSelectButtonPrefab, new Vector3(0, 0, 0), Quaternion.identity, content.transform);
-        bonusStageSelectButton = bonusStageSelectButtonGameObject.GetComponent<BonusStageSelectButton>();
-        bonusStageSelectButton.Initialize(totalStageCount, totalStageCount, playCount);
-    }
+		GameObject bonusStageSelectButtonGameObject = Instantiate(bonusStageSelectButtonPrefab, new Vector3(0, 0, 0), Quaternion.identity, content.transform);
+		bonusStageSelectButton = bonusStageSelectButtonGameObject.GetComponent<BonusStageSelectButton>();
+		bonusStageSelectButton.Initialize(totalStageCount, totalStageCount, playCount);
+	}
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            //bonusStageSelectButton.Reduction();
-            stageSelectButtons[1].Reduction();
-        }
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			//bonusStageSelectButton.Reduction();
+			stageSelectButtons[1].Reduction();
+		}
 
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            //bonusStageSelectButton.Expansion();
-            stageSelectButtons[1].Expansion();
-        }
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			//bonusStageSelectButton.Expansion();
+			stageSelectButtons[1].Expansion();
+		}
 
-        if (Input.GetKeyDown(KeyCode.U))
-        {
-            bonusStageSelectButton.AdvanceUnlock();
-        }
-    }
+		if (Input.GetKeyDown(KeyCode.U))
+		{
+			bonusStageSelectButton.AdvanceUnlock();
+		}
+	}
 
-    IEnumerator Direction()
-    {
-        StageSelectButton currentButton = stageSelectButtons.FirstOrDefault(button => button.ButtonNumber == playCount);
+	/// <summary>
+	/// 演出
+	/// </summary>
+	/// <returns></returns>
+	private IEnumerator Direction()
+	{
+		StageSelectButton currentButton = stageSelectButtons.FirstOrDefault(button => button.ButtonNumber == playCount);
 
-        if (currentButton != null)
-        {
-            scrollRect.ScrollToCentering(currentButton.gameObject, 1);
+		if (currentButton != null)
+		{
+			bool isCurrentButtonCompleted = false;
+			scrollRect.ScrollToCentering(currentButton.gameObject, 1, () =>
+			{
+				isCurrentButtonCompleted = true;
+			});
+			yield return new WaitUntil(() => isCurrentButtonCompleted);
 
-            int i = 0;
-            yield return new WaitWhile(() =>
-            {
-                i++;
-                currentButton.SetVerticalBarGauge(i);
-                return i < 100;
-            });
+			int i = 0;
+			yield return new WaitWhile(() =>
+			{
+				i++;
+				currentButton.SetVerticalBarGauge(i);
+				return i < 100;
+			});
 
-            StageSelectButton nextButton = stageSelectButtons.FirstOrDefault(button => button.ButtonNumber == playCount + 1);
-            if (nextButton != null)
-            {
-                scrollRect.ScrollToCentering(nextButton.gameObject, 1);
-                nextButton.SetFrameLineColor(Color.red);
-            }
-        }
+			StageSelectButton nextButton = stageSelectButtons.FirstOrDefault(button => button.ButtonNumber == playCount + 1);
+			if (nextButton != null)
+			{
+				bool isNextButtonCompleted = false;
+				scrollRect.ScrollToCentering(nextButton.gameObject, 1, () =>
+				{
+					isNextButtonCompleted = true;
+				});
+				yield return new WaitUntil(() => isNextButtonCompleted);
 
-        yield return null;
-    }
+				nextButton.SetFrameLineColor(Color.red);
+			}
+		}
+
+		yield return null;
+	}
 }
