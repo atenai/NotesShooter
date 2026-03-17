e85552933554a10df68fe79d1f9fafa5b200b099
atenai <alfamod47@yahoo.co.jp>
2026-03-17 14:05:20 +0900

    バーチャルパッドでカメラを操作するように修正

--- Assets/NotesShooter/Script/PlayerScript/FPSCamera.cs
+++ Assets/NotesShooter/Script/PlayerScript/FPSCamera.cs
@@ -2,113 +2,115 @@
 
 public class FPSCamera : MonoBehaviour
 {
-    //シングルトンで作成（ゲーム中に１つのみにする）
-    private static FPSCamera singletonInstance = null;
-    public static FPSCamera SingletonInstance => singletonInstance;
+	//シングルトンで作成（ゲーム中に１つのみにする）
+	private static FPSCamera singletonInstance = null;
+	public static FPSCamera SingletonInstance => singletonInstance;
 
-    [Tooltip("X軸のカメラの回転スピード")]
-    [Range(50, 150)][SerializeField] private float cameraSpeedX = 100;
-    [Tooltip("Y軸のカメラの回転スピード")]
-    [Range(50, 150)][SerializeField] private float cameraSpeedY = 100;
-    [Tooltip("ローカルで計算する為のX軸のカメラの回転スピード")]
-    private float localCameraSpeedX;
-    [Tooltip("ローカルで計算する為のY軸のカメラの回転スピード")]
-    private float localCameraSpeedY;
-    [Tooltip("カメラのスピードを遅くする")]
-    [Range(1.0f, 4.0f)] private float slowDownCameraSpeed = 2.0f;
-    [Tooltip("レイの長さ")]
-    [SerializeField] private float range = 100.0f;
-    [Tooltip("レティクルの中心点（レイキャスト）にターゲットがヒットしてるか？")]
-    private bool isTargethit = false;
-    public bool IsTargethit => isTargethit;
-    [Tooltip("横回転(Y)はプレイヤーキャラクターの座標位置を使っている(Player_RotY)")]
-    private Transform playerTransform;
-    [Tooltip("縦回転(X)はカメラの座標位置を使っている(FPSCamera_RotX)")]
-    private Transform cameraTransform;
+	[Tooltip("X軸のカメラの回転スピード")]
+	[Range(50, 150)][SerializeField] private float cameraSpeedX = 100;
+	[Tooltip("Y軸のカメラの回転スピード")]
+	[Range(50, 150)][SerializeField] private float cameraSpeedY = 100;
+	[Tooltip("ローカルで計算する為のX軸のカメラの回転スピード")]
+	private float localCameraSpeedX;
+	[Tooltip("ローカルで計算する為のY軸のカメラの回転スピード")]
+	private float localCameraSpeedY;
+	[Tooltip("カメラのスピードを遅くする")]
+	[Range(1.0f, 4.0f)] private float slowDownCameraSpeed = 2.0f;
+	[Tooltip("レイの長さ")]
+	[SerializeField] private float range = 100.0f;
+	[Tooltip("レティクルの中心点（レイキャスト）にターゲットがヒットしてるか？")]
+	private bool isTargethit = false;
+	public bool IsTargethit => isTargethit;
+	[Tooltip("横回転(Y)はプレイヤーキャラクターの座標位置を使っている(Player_RotY)")]
+	private Transform playerTransform;
+	[Tooltip("縦回転(X)はカメラの座標位置を使っている(FPSCamera_RotX)")]
+	private Transform cameraTransform;
 
-    void Awake()
-    {
-        //staticな変数instanceはメモリ領域は確保されていますが、初回では中身が入っていないので、中身を入れます。
-        if (singletonInstance == null)
-        {
-            singletonInstance = this;//thisというのは自分自身のインスタンスという意味になります。この場合、Playerのインスタンスという意味になります。
-        }
-        else
-        {
-            Destroy(this.gameObject);//中身がすでに入っていた場合、自身のインスタンスがくっついているゲームオブジェクトを破棄します。
-        }
-    }
+	void Awake()
+	{
+		//staticな変数instanceはメモリ領域は確保されていますが、初回では中身が入っていないので、中身を入れます。
+		if (singletonInstance == null)
+		{
+			singletonInstance = this;//thisというのは自分自身のインスタンスという意味になります。この場合、Playerのインスタンスという意味になります。
+		}
+		else
+		{
+			Destroy(this.gameObject);//中身がすでに入っていた場合、自身のインスタンスがくっついているゲームオブジェクトを破棄します。
+		}
+	}
 
-    void Start()
-    {
-        playerTransform = transform.parent;
-        cameraTransform = this.GetComponent<Transform>();
-    }
+	void Start()
+	{
+		playerTransform = transform.parent;
+		cameraTransform = this.GetComponent<Transform>();
+	}
 
-    void FixedUpdate()
-    {
-        float x_Rotation = Input.GetAxis("Mouse X");
-        float y_Rotation = Input.GetAxis("Mouse Y");
-        MouseCamera(new Vector2(x_Rotation, y_Rotation));
-    }
+	void FixedUpdate()
+	{
+		//float x_Rotation = Input.GetAxis("Mouse X");
+		//float y_Rotation = Input.GetAxis("Mouse Y");
+		float x_Rotation = PlayerUI.SingletonInstance.FloatingJoystick.Horizontal;
+		float y_Rotation = PlayerUI.SingletonInstance.FloatingJoystick.Vertical;
+		MouseCamera(new Vector2(x_Rotation, y_Rotation));
+	}
 
-    void MouseCamera(Vector2 angles)
-    {
-        float x_Rotation = angles.x;
-        float y_Rotation = angles.y;
+	void MouseCamera(Vector2 angles)
+	{
+		float x_Rotation = angles.x;
+		float y_Rotation = angles.y;
 
-        localCameraSpeedX = cameraSpeedX;
-        localCameraSpeedY = cameraSpeedY;
-        isTargethit = false;
+		localCameraSpeedX = cameraSpeedX;
+		localCameraSpeedY = cameraSpeedY;
+		isTargethit = false;
 
-        //ターゲットにあたった際にカメラを遅くする処理
-        Ray ray = new Ray(this.transform.position, this.transform.forward);
-        Debug.DrawRay(ray.origin, ray.direction * range, Color.gray, 1.0f);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, range) == true) // もしRayを投射して何らかのコライダーに衝突したら
-        {
-            if (hit.collider.gameObject.CompareTag("BlueTarget") || hit.collider.gameObject.CompareTag("PurpleTarget") || hit.collider.gameObject.CompareTag("RedTarget"))//※間違ってオブジェクトの設定にレイヤーとタグを間違えるなよおれｗ
-            {
-                //カメラの速さを遅くする
-                localCameraSpeedX = cameraSpeedX / slowDownCameraSpeed;
-                localCameraSpeedY = cameraSpeedY / slowDownCameraSpeed;
-                isTargethit = true;
-            }
-        }
+		//ターゲットにあたった際にカメラを遅くする処理
+		Ray ray = new Ray(this.transform.position, this.transform.forward);
+		Debug.DrawRay(ray.origin, ray.direction * range, Color.gray, 1.0f);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, range) == true) // もしRayを投射して何らかのコライダーに衝突したら
+		{
+			if (hit.collider.gameObject.CompareTag("BlueTarget") || hit.collider.gameObject.CompareTag("PurpleTarget") || hit.collider.gameObject.CompareTag("RedTarget"))//※間違ってオブジェクトの設定にレイヤーとタグを間違えるなよおれｗ
+			{
+				//カメラの速さを遅くする
+				localCameraSpeedX = cameraSpeedX / slowDownCameraSpeed;
+				localCameraSpeedY = cameraSpeedY / slowDownCameraSpeed;
+				isTargethit = true;
+			}
+		}
 
-        //マウスXの入力量 × カメラのスピード × 時間 = の値をX回転の量にする
-        playerTransform.transform.Rotate(0, x_Rotation * localCameraSpeedX * Time.deltaTime, 0);
+		//マウスXの入力量 × カメラのスピード × 時間 = の値をX回転の量にする
+		playerTransform.transform.Rotate(0, x_Rotation * localCameraSpeedX * Time.deltaTime, 0);
 
-        float cameraAngles = cameraTransform.transform.localEulerAngles.x;
-        const float lookingUpLimit = 360.0f;//変えてはいけない数値
-        float lookingUp = 324.0f;//減らしていくほど上を見れる範囲が広がる
-        const float lookingDownLimit = -10.0f;//変えてはいけない数値
-        float lookingDown = 79.0f;//増やしていくほど下を見れる範囲が広がる
+		float cameraAngles = cameraTransform.transform.localEulerAngles.x;
+		const float lookingUpLimit = 360.0f;//変えてはいけない数値
+		float lookingUp = 324.0f;//減らしていくほど上を見れる範囲が広がる
+		const float lookingDownLimit = -10.0f;//変えてはいけない数値
+		float lookingDown = 79.0f;//増やしていくほど下を見れる範囲が広がる
 
-        if (lookingUp < cameraAngles && cameraAngles < lookingUpLimit || lookingDownLimit < cameraAngles && cameraAngles < lookingDown)//ここの数値を変えればカメラの上下の止まる限界値が変わる
-        {
-            //マウスYの入力量 × カメラのスピード × 時間 = の値をY回転の量にする
-            cameraTransform.transform.Rotate(-y_Rotation * localCameraSpeedY * Time.deltaTime, 0, 0);
-        }
-        else
-        {
-            if (300 < cameraAngles)
-            {
-                if (Input.GetAxis("Mouse Y") < 0)
-                {
-                    //マウスYの入力量 × カメラのスピード × 時間 = の値をY回転の量にする
-                    cameraTransform.transform.Rotate(-y_Rotation * localCameraSpeedY * Time.deltaTime, 0, 0);
-                }
-            }
-            else
-            {
-                if (0 < Input.GetAxis("Mouse Y"))
-                {
-                    //マウスYの入力量 × カメラのスピード × 時間 = の値をY回転の量にする
-                    cameraTransform.transform.Rotate(-y_Rotation * localCameraSpeedY * Time.deltaTime, 0, 0);
-                }
+		if (lookingUp < cameraAngles && cameraAngles < lookingUpLimit || lookingDownLimit < cameraAngles && cameraAngles < lookingDown)//ここの数値を変えればカメラの上下の止まる限界値が変わる
+		{
+			//マウスYの入力量 × カメラのスピード × 時間 = の値をY回転の量にする
+			cameraTransform.transform.Rotate(-y_Rotation * localCameraSpeedY * Time.deltaTime, 0, 0);
+		}
+		else
+		{
+			if (300 < cameraAngles)
+			{
+				if (Input.GetAxis("Mouse Y") < 0)
+				{
+					//マウスYの入力量 × カメラのスピード × 時間 = の値をY回転の量にする
+					cameraTransform.transform.Rotate(-y_Rotation * localCameraSpeedY * Time.deltaTime, 0, 0);
+				}
+			}
+			else
+			{
+				if (0 < Input.GetAxis("Mouse Y"))
+				{
+					//マウスYの入力量 × カメラのスピード × 時間 = の値をY回転の量にする
+					cameraTransform.transform.Rotate(-y_Rotation * localCameraSpeedY * Time.deltaTime, 0, 0);
+				}
 
-            }
-        }
-    }
+			}
+		}
+	}
 }
--- Assets/NotesShooter/Script/UIScript/PlayerUI.cs
+++ Assets/NotesShooter/Script/UIScript/PlayerUI.cs
@@ -5,102 +5,106 @@ using UnityEngine.UI;
 
 public class PlayerUI : MonoBehaviour
 {
-    //シングルトンで作成（ゲーム中に１つのみにする）
-    private static PlayerUI singletonInstance = null;
-    public static PlayerUI SingletonInstance => singletonInstance;
+	//シングルトンで作成（ゲーム中に１つのみにする）
+	private static PlayerUI singletonInstance = null;
+	public static PlayerUI SingletonInstance => singletonInstance;
 
-    [Tooltip("レティクル画像")]
-    [SerializeField] Image imageReticle;
-    [Tooltip("クロスヘア画像")]
-    [SerializeField] Image imageCrossHair;
+	[Tooltip("レティクル画像")]
+	[SerializeField] Image imageReticle;
+	[Tooltip("クロスヘア画像")]
+	[SerializeField] Image imageCrossHair;
 
-    [Tooltip("ポーズ")]
-    bool isPause = false;
-    public bool IsPause => isPause;
-    [Tooltip("ポーズ画像")]
-    [SerializeField] GameObject panelPause;
-    [SerializeField] AudioSource audioSource;
+	[Tooltip("ポーズ")]
+	bool isPause = false;
+	public bool IsPause => isPause;
+	[Tooltip("ポーズ画像")]
+	[SerializeField] GameObject panelPause;
+	[SerializeField] AudioSource audioSource;
 
-    [SerializeField] BaseGunUI redGunUI;
-    public BaseGunUI RedGunUI
-    {
-        get { return redGunUI; }
-        set { redGunUI = value; }
-    }
+	[SerializeField] BaseGunUI redGunUI;
+	public BaseGunUI RedGunUI
+	{
+		get { return redGunUI; }
+		set { redGunUI = value; }
+	}
 
-    [SerializeField] BaseGunUI blueGunUI;
-    public BaseGunUI BlueGunUI
-    {
-        get { return blueGunUI; }
-        set { blueGunUI = value; }
-    }
+	[SerializeField] BaseGunUI blueGunUI;
+	public BaseGunUI BlueGunUI
+	{
+		get { return blueGunUI; }
+		set { blueGunUI = value; }
+	}
 
-    [SerializeField] BaseGunUI purpleGunUI;
-    public BaseGunUI PurpleGunUI
-    {
-        get { return purpleGunUI; }
-        set { purpleGunUI = value; }
-    }
+	[SerializeField] BaseGunUI purpleGunUI;
+	public BaseGunUI PurpleGunUI
+	{
+		get { return purpleGunUI; }
+		set { purpleGunUI = value; }
+	}
 
-    void Awake()
-    {
-        //staticな変数instanceはメモリ領域は確保されていますが、初回では中身が入っていないので、中身を入れます。
-        if (singletonInstance == null)
-        {
-            singletonInstance = this;//thisというのは自分自身のインスタンスという意味になります。この場合、Playerのインスタンスという意味になります。
-        }
-        else
-        {
-            Destroy(this.gameObject);//中身がすでに入っていた場合、自身のインスタンスがくっついているゲームオブジェクトを破棄します。
-        }
-    }
+	[Tooltip("ジョイスティック")]
+	[SerializeField] FloatingJoystick floatingJoystick;
+	public FloatingJoystick FloatingJoystick => floatingJoystick;
+
+	void Awake()
+	{
+		//staticな変数instanceはメモリ領域は確保されていますが、初回では中身が入っていないので、中身を入れます。
+		if (singletonInstance == null)
+		{
+			singletonInstance = this;//thisというのは自分自身のインスタンスという意味になります。この場合、Playerのインスタンスという意味になります。
+		}
+		else
+		{
+			Destroy(this.gameObject);//中身がすでに入っていた場合、自身のインスタンスがくっついているゲームオブジェクトを破棄します。
+		}
+	}
 
 
-    void Start()
-    {
-        imageReticle.color = new Color(255.0f, 255.0f, 255.0f, 150);
-        imageCrossHair.color = new Color(255.0f, 255.0f, 255.0f, 150);
-    }
+	void Start()
+	{
+		imageReticle.color = new Color(255.0f, 255.0f, 255.0f, 150);
+		imageCrossHair.color = new Color(255.0f, 255.0f, 255.0f, 150);
+	}
 
-    void Update()
-    {
-        if (FPSCamera.SingletonInstance.IsTargethit == true)
-        {
-            imageReticle.color = new Color(255, 0, 0, 150);
-            imageCrossHair.color = new Color(255, 0, 0, 150);
-        }
-        else
-        {
-            imageReticle.color = new Color(255, 255, 255, 150);
-            imageCrossHair.color = new Color(255, 255, 255, 150);
-        }
+	void Update()
+	{
+		if (FPSCamera.SingletonInstance.IsTargethit == true)
+		{
+			imageReticle.color = new Color(255, 0, 0, 150);
+			imageCrossHair.color = new Color(255, 0, 0, 150);
+		}
+		else
+		{
+			imageReticle.color = new Color(255, 255, 255, 150);
+			imageCrossHair.color = new Color(255, 255, 255, 150);
+		}
 
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            Pause();
-        }
-    }
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			Pause();
+		}
+	}
 
-    /// <summary>
-    /// ポーズ
-    /// </summary>
-    void Pause()
-    {
-        isPause = isPause ? false : true;
+	/// <summary>
+	/// ポーズ
+	/// </summary>
+	void Pause()
+	{
+		isPause = isPause ? false : true;
 
-        if (isPause == true)
-        {
-            Time.timeScale = 0f;
-            panelPause.SetActive(true);
-            // 一時停止
-            audioSource.Pause();
-        }
-        else
-        {
-            Time.timeScale = 1f;
-            panelPause.SetActive(false);
-            // 一時停止解除
-            audioSource.UnPause();
-        }
-    }
+		if (isPause == true)
+		{
+			Time.timeScale = 0f;
+			panelPause.SetActive(true);
+			// 一時停止
+			audioSource.Pause();
+		}
+		else
+		{
+			Time.timeScale = 1f;
+			panelPause.SetActive(false);
+			// 一時停止解除
+			audioSource.UnPause();
+		}
+	}
 }
